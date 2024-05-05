7cbd15f9ba5445c30c1b38fae11675d79dcd5a07
Vikrant <vikrant.sangwan@ibm.com>
2024-05-05 23:00:01 +0530

    complete signup and proxy addition

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server : {
+    // each time  /api add localhost:3000 in the begining
+    proxy : {
+      '/api' : {
+        target : 'http://localhost:3000',
+        secure: false
+      }
+    }
+  },
   plugins: [react()],
 })
