d077d9b97458991eb58be30cff59f14b2ef12687
VikrantSangwan <v.sangwan9876@gmail.com>
2024-05-09 17:02:10 +0530

    adding comment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,6 +8,7 @@ import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+    {/* Adding the persitor to the root app  */}
     <PersistGate loading={null} persistor={persistor}>
       <App />
     </PersistGate>
