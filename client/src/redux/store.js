22b426b0cac2019dfb6127743e957c01aab6cd33
Vikrant <vikrant.sangwan@ibm.com>
2024-05-09 16:52:34 +0530

    adding redux persit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import {persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
+// Combining all the reducers
+const rootReducer = combineReducers({ user: userReducer });
+
+// Creating a basic configuration for persistor
+const persistConfig = {
+  key : 'root',
+  storage,
+  version : 1,
+}
+
+// Creating a persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Adding a persist reducer to the store
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+// Exporting the persitor
+export const persistor = persistStore(store);
+
