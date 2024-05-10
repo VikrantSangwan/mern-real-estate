ccc1c85a751a18955a9269a6f5d4a4a5070a7c32
VikrantSangwan <v.sangwan9876@gmail.com>
2024-05-11 00:48:15 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,17 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { useRef } from "react";
+import {
+  getStorage,
+  uploadBytesResumable,
+  ref,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../firebase.js";
 
 export default function Profile() {
+  const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
+  const [file, setfile] = useState(null);
+  const [fileUploadPerc, setfileUploadPerc] = useState(0);
+  const [fileUploadError, setfileUploadError] = useState(false);
+  const [formData, setformData] = useState({});
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    // Initialize Firebase storage service using the Firebase app instance
+    const storage = getStorage(app);
+    // Generate a unique file name by combining the current timestamp with the original file name
+    const fileName = new Date().getTime() + file.name;
+    // Create a reference to the location where the file will be stored in Firebase Storage.
+    const storageRef = ref(storage, fileName);
+    // Set up a listener for state changes in the upload task to track the upload progress
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setfileUploadPerc(Math.round(progress));
+      },
+      (error) => {
+        setfileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setformData({ ...formData, avatar: downloadURL });
+        });
+      }
+    );
+  };
+
+  // file base configuration for adding images file
+  // allow read;
+  // allow write: if
+  // request.resource.size < 2 * 1025 * 1024 &&
+  // request.resource.contentType.matches('image/.*')
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
+        {/* creating a reference and triggering it via on click event of image icon below  */}
+        <input
+          type="file"
+          accept="images/*"
+          ref={fileRef}
+          hidden
+          onChange={(e) => setfile(e.target.files[0])}
+        />
         <img
-          src={currentUser.avatar}
+          src={formData.avatar || currentUser.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center"
+          onClick={() => fileRef.current.click()}
         />
+        <p className="text-sm self-center">
+          {fileUploadError ? (
+            <span className="text-red-700">Error while uploading file (image size should be less than 2 MB)</span>
+          ) : fileUploadPerc > 0 && fileUploadPerc < 100 ? (
+            <span className="text-slate-700">{`Uploading Image ${fileUploadPerc}%`}</span>
+          ) : fileUploadPerc === 100 ? (
+            <span className="text-green-700">Image Uploaded Successfully</span>
+          ) : (
+            ""
+          )}
+        </p>
         <input
           type="text"
           id="username"
